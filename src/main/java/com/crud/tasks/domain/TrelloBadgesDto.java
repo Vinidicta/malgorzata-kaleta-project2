a56333facb9eb91804f3ec85be73f4edcfc7c91e
Vinidicta <malgorzatakaleta1990@gmail.com>
2018-04-24 20:40:30 +0200

    18.3 Trello Badges

---
+++ src/main/java/com/crud/tasks/domain/TrelloBadgesDto.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloBadgesDto {
+    private int votes;
+    private TrelloAttachmentsByTypeDto attachments;
+}
